bd23a5f1d96283e5cec6b862f8ba404cf54d1d83
Alex Fisher <alex@promptvault.com>
2026-06-29 01:56:22 +0000

    Clean repo: ignore build artifacts

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
